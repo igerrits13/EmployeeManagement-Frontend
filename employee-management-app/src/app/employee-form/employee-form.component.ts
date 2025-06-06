b6edcdd65b9fa17acae12a2da2684ec6595f99f0
igerrits13 <77903575+igerrits13@users.noreply.github.com>
2025-06-06 15:44:39 -0600

    Add routing after creating employee and error handling

--- employee-management-app/src/app/employee-form/employee-form.component.ts
+++ employee-management-app/src/app/employee-form/employee-form.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Employee } from '../../models/employee';
 import { EmployeeService } from '../employee.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employee-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './employee-form.component.html',
   styleUrl: './employee-form.component.css',
 })
@@ -21,14 +23,27 @@ export class EmployeeFormComponent {
     position: '',
   };
 
+  errorMessage: string = '';
+
   // Initialize the employee service
-  constructor(private employeeService: EmployeeService) {}
+  constructor(
+    private employeeService: EmployeeService,
+    private router: Router
+  ) {}
 
   // Submit function
   onSubmit(): void {
     // Create a new employee via the employee service
-    this.employeeService
-      .createEmployee(this.employee)
-      .subscribe((result) => console.log(result));
+    this.employeeService.createEmployee(this.employee).subscribe({
+      // If successful, route to home page
+      next: () => {
+        this.router.navigate(['/']);
+      },
+      // otherwise, display the error
+      error: (err) => {
+        console.error(err);
+        this.errorMessage = `Error (${err.status})`;
+      },
+    });
   }
 }
