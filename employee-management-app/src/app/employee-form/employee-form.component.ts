d962165a8e47c2fe7a21815f3d704c9927c32f61
igerrits13 <77903575+igerrits13@users.noreply.github.com>
2025-06-06 13:40:02 -0600

    Add simple navigation bar and form data

--- employee-management-app/src/app/employee-form/employee-form.component.ts
+++ employee-management-app/src/app/employee-form/employee-form.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Employee } from '../../models/employee';
 
 @Component({
   selector: 'app-employee-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './employee-form.component.html',
-  styleUrl: './employee-form.component.css'
+  styleUrl: './employee-form.component.css',
 })
 export class EmployeeFormComponent {
+  // Employee form to hold informaiton
+  employee: Employee = {
+    id: 0,
+    firstName: '',
+    lastName: '',
+    phone: '',
+    email: '',
+    position: '',
+  };
 
+  // Submit function
+  onSubmit(): void {
+    console.log(this.employee);
+  }
 }
